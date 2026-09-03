6051ce0d788d6e8bced6c78f6520fd4e9dd4778f
yoga220802 <yogaagustiansyah01@gmail.com>
2026-09-04 06:56:21 +0700

    fix(ui): suppress extension-induced body hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,9 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body suppressHydrationWarning className="min-h-full flex flex-col">
+        {children}
+      </body>
     </html>
   );
 }
